672fd4fdd5ad3add23cfa0d9ab2edf50c3e9aa89
Satyasarathi Das <97898201+sssbabu@users.noreply.github.com>
2025-06-06 09:59:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web" {
+/*resource "aws_instance" "web" {
   ami           = "ami-06031e2c49c278c8f"
   instance_type = "t2.micro"
    count=1
@@ -7,3 +7,4 @@ resource "aws_instance" "web" {
     test = "dev"
   }
 }
+*/
