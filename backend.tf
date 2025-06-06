81180aee8ca1526845c183e3ea8215ac41688374
Satyasarathi Das <97898201+sssbabu@users.noreply.github.com>
2025-06-06 09:18:06 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
  backend "s3" {
- bucket = "idream-test-terraform"
- key = "terraform.tfstate"
+ bucket = "satya-terraform-statefile-bucket"
+ key = "key/terraform.tfstate"
  region = "ap-south-1"
       }
 }
